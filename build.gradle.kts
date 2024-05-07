13d3e0a516910ed114860175554730ead7a489ae
StanHigh <stasntnew@gmail.com>
2024-05-07 14:24:22 +0200

    gradle versions updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ buildscript{
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     kotlin("kapt") version "1.9.22"
-    id("com.android.application") version "8.3.2" apply false
+    id("com.android.application") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     // for ktor serialization
     id ("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
